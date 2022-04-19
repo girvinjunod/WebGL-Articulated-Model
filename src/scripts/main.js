304d253d4a61a798cffaf54d9693e47f939b5d57
girvinjunod <girvinjunod@gmail.com>
2022-04-20 00:18:59 +0700

    style: change button layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,7 @@ import { GLUtils } from "./modules/GLUtils.js";
 import { load, TempObj } from "./utils/load.js";
 import { getFragShader } from "./shaders/fragmentShader.js";
 import { getVertShader } from "./shaders/vertexShader.js";
-import {saveBlob} from "./utils/screenshot.js";
+import { saveBlob } from "./utils/screenshot.js";
 
 window.onload = () => {
   let main = () => {
@@ -30,38 +30,35 @@ window.onload = () => {
 
       try {
         const fileName = ev.target.files[0].name;
-        if (fileName == "wolf.json"){
-          glUtil.setObjectName("Wolf"); 
-          document.getElementById("objName").innerHTML = "Wolf Object";
-        }else if (fileName == "person.json"){
-            glUtil.setObjectName("Human");
-            document.getElementById("objName").innerHTML = "Human Object";
-        }else if (fileName == "superhero.json"){
-            glUtil.setObjectName("Superhero");
-            document.getElementById("objName").innerHTML = "Superhero Object";
-        }else if (fileName == "platypus.json"){
-            glUtil.setObjectName("Platypus");
-            document.getElementById("objName").innerHTML = "Platypus Object";
-        }
-        else{
-            glUtilInstance.setObjectName("Unknown");
-            document.getElementById("objName").innerHTML = "Unknown Object";
+        if (fileName == "wolf.json") {
+          glUtil.setObjectName("Wolf");
+          document.getElementById("objName").innerHTML = "Wolf Model";
+        } else if (fileName == "person.json") {
+          glUtil.setObjectName("Human");
+          document.getElementById("objName").innerHTML = "Human Model";
+        } else if (fileName == "superhero.json") {
+          glUtil.setObjectName("Superhero");
+          document.getElementById("objName").innerHTML = "Superhero Model";
+        } else if (fileName == "platypus.json") {
+          glUtil.setObjectName("Platypus");
+          document.getElementById("objName").innerHTML = "Platypus Model";
+        } else {
+          glUtilInstance.setObjectName("Unknown");
+          document.getElementById("objName").innerHTML = "Unknown Model";
         }
         load(ev.target.files[0], setObject);
-      }catch (error) {
+      } catch (error) {
         console.log(error);
       }
-      
     });
 
     document.getElementById("animate-btn").onclick = () => {
       glUtil.setAnimationFlag();
     };
 
-    document.getElementById("saveImg-btn").onclick = () => { 
+    document.getElementById("saveImg-btn").onclick = () => {
       glUtil.needCapture = true;
     };
-
 
     document.getElementById("default-btn").onclick = () => {
       glUtil.setDefaultView();
@@ -197,10 +194,9 @@ window.onload = () => {
       if (glUtil.needCapture) {
         glUtil.changeNeedCapture();
         canvas.toBlob((blob) => {
-          saveBlob(blob,glUtil.objectName);
+          saveBlob(blob, glUtil.objectName);
         });
       }
-
     };
     requestAnimationFrame(render);
   };
